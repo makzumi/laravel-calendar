4abd1e25d632a4652a6ec4e7fd251c837a31372d
Ammar Almakzumi <makzumi@makzumi-mac-mini.local>
2014-06-16 13:15:26 -0500

    Testing service hook

--- src/Makzumi/Calendar/Calendar.php
+++ src/Makzumi/Calendar/Calendar.php
@@ -254,9 +254,7 @@ class Calendar {
 	private function buildBody() {
 		
 		$day = 1;
-		
 		$now_date = $this->year . '-' . $this->month . '-01';
-		
 		$startingDay = date('N', strtotime('first day of this month', strtotime($now_date)));
 
 		$monthLength = $this->days_month[$this->month - 1];
